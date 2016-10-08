a9ba5096cb267d88dd34489cfe74884d61a08de5
FluidCoding <sonnie130@gmail.com>
2016-10-08 18:16:28 -0400

    Snek

--- main.js
+++ main.js
@@ -23,54 +23,88 @@ var food = {
 }
 
 var snake ={
-  curD : D.RIGHT
+  curD : D.RIGHT,
+  body : [],
+  addSeg: function (x,y){
+    console.log("adding to ", x,y);
+    var seg = snakeSeg(x,y);
+    this.body.push(seg);
+  }
 }
-
-var snakeSeg = {
-  x: 4,
-  y: 4,
-  width: 5,
-  height: 5,
-  bods: []
+/*
+  Snake
+  body [
+    snakeseg,
+    snakeseg,
+    snakeseg
+  ]
+  tick -> move head shift segments into sibling
+      eat -> add one to pre shift end attach after shift
+*/
+var snakeSeg = function (_x,_y ) {
+  return {
+    x: _x,
+    y: _y,
+    width: 5,
+    height: 5
+  }
 }
-
+var lLast = 0;
 function render(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   // Check for Food And Self Destruct
 
-  // Move Snake
-  switch(snake.curD){
-    case D.LEFT:
-      snakeSeg.x -= snakeSeg.width;
-    break;
-    case D.RIGHT:
-      snakeSeg.x += snakeSeg.width;
-    break;
-    case D.DOWN:
-      snakeSeg.y += snakeSeg.height;
-    break;
-    case D.UP:
-      snakeSeg.y -= snakeSeg.height;
-    break;
-  }
   if(!food.display){
     food.x = Math.random() * (canvas.width);
-    food.y = Math.random() * (canvas.height);
+  //  food.y = Math.random() * (canvas.height);
+    food.y = 75;
     food.display = true;
   }else{
+    // Draw Food
     ctx.fillStyle = "white";
     ctx.fillRect(food.x, food.y, food.height,food.width);
-    if (food.x < snakeSeg.x + snakeSeg.width &&
-       food.x + food.width > snakeSeg.x &&
-       food.y < snakeSeg.y + snakeSeg.height &&
-       food.height + food.y > snakeSeg.y) {
+    // If Head Eats Food
+    if (food.x < snake.body[0].x + snake.body[0].width &&
+       food.x + food.width > snake.body[0].x &&
+       food.y < snake.body[0].y + snake.body[0].height &&
+       food.height + food.y > snake.body[0].y) {
         food.eat();
+        // Add to tail
+        snake.addSeg(snake.body[snake.body.length-1].x,snake.body[snake.body.length-1].y);
+        // Dont move last seg
+        lLast = 1;
     }
   }
+  // Move Snake Body, leave last segment if its new
 
-    // Draw Snake
-    ctx.fillStyle = "green";
-    ctx.fillRect(snakeSeg.x, snakeSeg.y, snakeSeg.height,snakeSeg.width);
+  for(var i = snake.body.length-lLast-1; i>0; i--){
+    snake.body[i].x = snake.body[i-1].x;
+    snake.body[i].y = snake.body[i-1].y;
+  }
+
+
+  // Move Head
+
+  switch(snake.curD){
+    case D.LEFT:
+      snake.body[0].x -= snake.body[0].width;
+    break;
+    case D.RIGHT:
+      snake.body[0].x += snake.body[0].width;
+    break;
+    case D.DOWN:
+      snake.body[0].y += snake.body[0].height;
+    break;
+    case D.UP:
+      snake.body[0].y -= snake.body[0].height;
+    break;
+  }
+  // Draw Snake
+  ctx.fillStyle = "green";
+  snake.body.forEach(function (seg,i){
+    ctx.fillRect(seg.x, seg.y, seg.height, seg.width);
+  });
+  lLast = 0;
 }
 
 function tick(){
@@ -95,8 +129,15 @@ document.body.onload = function () {
       case 40:  // Down
         snake.curD = D.DOWN;
       break;
+      case 0x20:
+        if(gTimer !== null) {clearInterval(gTimer); gTimer = null;}
+        else                {gTimer = setInterval(tick, 150);     }
+      break;
     }
   }
 
-  gTimer = setInterval(tick, 500);
+  snake.addSeg(10,20);
+  snake.addSeg(15,20);
+
+  gTimer = setInterval(tick, 150);
 }
